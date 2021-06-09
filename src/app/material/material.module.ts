05058a040e58481923b0fffe65ea8b2afe292fa1
krishna-fullstack-coder <krishna.fullstack.coder@gmail.com>
2021-06-09 19:03:24 +0530

    Added date time picker

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialPageComponent } from './material-page/material-page.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+
+// for date
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+//
+
+import { DatepickerComponent } from './material-page/datepicker/datepicker.component';
 
 @NgModule({
   declarations: [
-    MaterialPageComponent
+    MaterialPageComponent,
+    DatepickerComponent
   ],
   imports: [
     CommonModule,
-    MatTabsModule
+    FormsModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   exports: [
     MaterialPageComponent
